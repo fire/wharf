1065c3d341128982f634fecddd7c02fc16e93e77
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-10-17 18:20:43 -0700

    Add brotli to list of libraries.

---
+++ wharfex/lib/wharfex.ex
@@ -0,0 +1,18 @@
+defmodule Wharfex do
+  @moduledoc """
+  Documentation for Wharfex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wharfex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ wharfex/test/wharfex_test.exs
@@ -0,0 +1,8 @@
+defmodule WharfexTest do
+  use ExUnit.Case
+  doctest Wharfex
+
+  test "greets the world" do
+    assert Wharfex.hello() == :world
+  end
+end
