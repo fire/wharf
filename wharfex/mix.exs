a5c8fffc3be875b7cc40abae19eb825909182474
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-10-17 18:23:03 -0700

    Add zstd.

--- wharfex/mix.exs
+++ wharfex/mix.exs
@@ -21,7 +21,8 @@ defmodule Wharfex.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:brotli, "~> 0.1.4"}
+      {:brotli, "~> 0.1.4"},
+      {:zstd, "~> 0.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
